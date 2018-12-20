b4c8ebff92ba83f5ae47c3f1416493744d912f59
Sandhya023 <yadavshndhya@gmail.com>
2018-12-20 21:04:01 +0530

    #controllers

--- routes/api.js
+++ routes/api.js
@@ -2,41 +2,34 @@
 const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
-
-const players = [
-	{firstName:"eli", lastName:"manning", position:"qb", age:37, team:"nyg"},
-	{firstName:"tom", lastName:"brady", position:"qb", age:41, team:"nep"},
-	{firstName:"jj", lastName:"watt", position:"de", age:28, team:"hou"}
-]
-
-const teams = [
-	{name:"giants", city:"new york", conference:"nfc"},
-	{name:"patriots", city:"new england", conference:"afc"},
-	{name:"texans", city:"houston", conference:"afc"}
-]
-
-const db = {
-	team: teams,
-	player: players
-}
+const controllers = require('../controllers')
 
 router.get('/:resource', (req, res) => {
 	const resource = req.params.resource
+	const controller = controllers[resource]
 
-	const data = db[resource]
-	if (data == null){
+	if (controller == null){
 		res.json({
 			confirmation: 'fail',
-			message: 'Invalid request. Resource undefined.'
+			message: 'Invalid Resource'
 		})
-
 		return
 	}
 
-	res.json({
-		confirmation: 'success',
-		data: data
+	controller.get()
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+			data: data
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
 	})
 })
+
 
 module.exports = router

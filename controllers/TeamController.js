d579fc4f89f75565f31d0a7ac0caee0b4c4da073
Sandhya023 <yadavshndhya@gmail.com>
2018-12-25 15:10:14 +0530

    Get request

--- controllers/TeamController.js
+++ controllers/TeamController.js
@@ -12,6 +12,17 @@ module.exports = {
 				reject(err)
 			})
 		})
-	}
+	},
 
+	getById: (id) => {
+		return new Promise((resolve, reject) => {
+			Team.findById(id)
+			.then(data => {
+				resolve(data)
+			})
+			.catch(err => {
+				reject(new Error('Team ' + id +' not found'))
+			})
+		})
+	}
 }
--- routes/api.js
+++ routes/api.js
@@ -4,9 +4,11 @@ const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 const controllers = require('../controllers')
 
+// GET, POST, PUT, DELETE
 router.get('/:resource', (req, res) => {
 	const resource = req.params.resource
 	const controller = controllers[resource]
+	const filters = req.query
 
 	if (controller == null){
 		res.json({
@@ -16,7 +18,7 @@ router.get('/:resource', (req, res) => {
 		return
 	}
 
-	controller.get()
+	controller.get(filters)
 	.then(data => {
 		res.json({
 			confirmation: 'success',
@@ -28,6 +30,36 @@ router.get('/:resource', (req, res) => {
 			confirmation: 'fail',
 			message: err.message
 		})
+	})
+})
+
+router.get('/:resource/:id', (req, res) => {
+	const resource = req.params.resource
+	const id = req.params.id
+
+	const controller = controllers[resource]
+	if (controller == null){
+		res.json({
+			confirmation: 'fail',
+			message: 'Invalid Resource'
+		})
+
+		return
+	}
+
+	controller.getById(id)
+	.then(data => {
+		res.json({
+			confirmation: 'success',
+			data: data
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+
 	})
 })
 
